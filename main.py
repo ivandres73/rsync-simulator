05d0bb953eb6aa62e71ca49cf568b51a8b79f0a5
Ivan C <ivandres73@unitec.edu>
2021-01-11 23:02:36 -0600

    calling rsync function from main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys
+import rsync_algorithm as rsync
 
 def main(argv):
     if (len(argv) <= 1):
         err_msg = "\n\tUsage: " + argv[0] + " <src-dir> <dst-dir>\n"
         sys.exit(err_msg)
+    rsync.rsync()
 
 argv=sys.argv
 main(argv)
