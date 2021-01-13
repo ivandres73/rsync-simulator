dcf002d15bc97685a273091e388cf8448f6a7b83
Ivan C <ivandres73@unitec.edu>
2021-01-12 23:36:55 -0600

    creating Directory class n testing functions

--- dir_analyzer.py
+++ dir_analyzer.py
@@ -8,3 +8,24 @@ def list_items(dir_path):
                 print("/")
             else:
                 print("")
+
+class Directory:
+    def __init__(self, dir_path):
+        self.items = os.scandir(dir_path)
+        self.dir_path = dir_path
+
+    def getName(self):
+        return self.dir_path
+
+    def getDirs(self):
+        dirs = []
+        with self.items as entries:
+            for entry in entries:
+                if entry.is_dir():
+                    dirs.append(entry)
+        return dirs
+
+    def getDirSize(self, dir_path):
+        temp_dir = self.dir_path + dir_path
+        print(temp_dir, "= ")
+        list_items(temp_dir)
--- rsync_algorithm.py
+++ rsync_algorithm.py
@@ -3,3 +3,7 @@ import dir_analyzer as da
 def rsync(source, destin):
     print("reventuki")
     da.list_items(source)
+    d1 = da.Directory(source)
+    print(d1.getName())
+    # print(d1.getDirs())
+    d1.getDirSize('.git/')
